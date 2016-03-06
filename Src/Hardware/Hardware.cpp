b84252693aa8aaa6cd0eb562af683335fc59cf2a
Thomas Breuer <thomas.breuer@h-brs.de>
2016-03-06 12:08:59 +0100

    Initial version

---
+++ Src/Hardware/Hardware.cpp
@@ -0,0 +1,12 @@
+//*******************************************************************
+/*!
+\file   Hardware/Hardware.cpp
+\author Thomas Breuer
+\date   06.03.2016
+
+\brief Summary of Hardware related code
+*/
+
+//*******************************************************************
+
+// EOF
